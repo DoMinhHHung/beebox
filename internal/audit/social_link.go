39f41302c95221162f9440049c871fad9fdbcd95
Đỗ Minh Hùng <92613966+DoMinhHHung@users.noreply.github.com>
2026-08-19 18:33:27 +0700

    feat: add social link audit vocabulary

---
+++ internal/audit/social_link.go
@@ -0,0 +1,9 @@
+package audit
+
+const (
+	ActionSocialLinkSucceeded = "authentication.social.link_succeeded"
+	ActionSocialLinkDenied    = "authentication.social.link_denied"
+
+	ResourceCategorySocialLink = "social_link"
+	SourceInternalSocialLink    = "internal_social_link"
+)
