0e2b636ca27edf467950a904051e6ccd4c8a8a68
Đỗ Minh Hùng <92613966+DoMinhHHung@users.noreply.github.com>
2026-08-20 11:28:17 +0700

    test: isolate social link migration history

--- internal/platform/migration/social_link_migration_integration_test.go
+++ internal/platform/migration/social_link_migration_integration_test.go
@@ -37,10 +37,10 @@ func TestSocialLinkMigrationUpgradesVersion15AndEnforcesBindings(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if entry.Name() != "00016_social_account_linking.sql" && entry.Name() != "00017_social_account_management.sql" {
+		if entry.Name() != "00016_social_account_linking.sql" && entry.Name() != "00017_social_account_management.sql" && entry.Name() != "00018_passkeys.sql" {
 			version15[entry.Name()] = &fstest.MapFile{Data: data}
 		}
-		if entry.Name() != "00017_social_account_management.sql" {
+		if entry.Name() != "00017_social_account_management.sql" && entry.Name() != "00018_passkeys.sql" {
 			version16[entry.Name()] = &fstest.MapFile{Data: data}
 		}
 	}
