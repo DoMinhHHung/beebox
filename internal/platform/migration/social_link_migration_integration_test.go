ce5ca6f51ee83026ac99c2f72a3fd11f0a3b886b
Đỗ Minh Hùng <92613966+DoMinhHHung@users.noreply.github.com>
2026-08-20 14:32:36 +0700

    test: exclude TOTP from social link history fixtures

--- internal/platform/migration/social_link_migration_integration_test.go
+++ internal/platform/migration/social_link_migration_integration_test.go
@@ -37,10 +37,10 @@ func TestSocialLinkMigrationUpgradesVersion15AndEnforcesBindings(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if entry.Name() != "00016_social_account_linking.sql" && entry.Name() != "00017_social_account_management.sql" && entry.Name() != "00018_passkeys.sql" {
+		if entry.Name() != "00016_social_account_linking.sql" && entry.Name() != "00017_social_account_management.sql" && entry.Name() != "00018_passkeys.sql" && entry.Name() != "00019_totp_mfa.sql" {
 			version15[entry.Name()] = &fstest.MapFile{Data: data}
 		}
-		if entry.Name() != "00017_social_account_management.sql" && entry.Name() != "00018_passkeys.sql" {
+		if entry.Name() != "00017_social_account_management.sql" && entry.Name() != "00018_passkeys.sql" && entry.Name() != "00019_totp_mfa.sql" {
 			version16[entry.Name()] = &fstest.MapFile{Data: data}
 		}
 	}
