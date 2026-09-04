45ac35354b6153be424bf6b4450a346b05283f66
Đỗ Minh Hùng <92613966+DoMinhHHung@users.noreply.github.com>
2026-09-04 18:45:37 +0700

    beebox-fix: add accounts password_hash migration and gofmt

---
+++ beebox-projects/internal/interface/http/owner_test.go
@@ -0,0 +1,16 @@
+package httpapi
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/DoMinhHHung/beebox/beebox-projects/internal/application"
+	"github.com/DoMinhHHung/beebox/beebox-projects/internal/domain"
+	"github.com/DoMinhHHung/beebox/beebox-projects/internal/infrastructure/crypto"
+	"github.com/google/uuid"
+)
