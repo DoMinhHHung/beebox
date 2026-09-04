36ff9f457da3ba28a0367ddd8e307ecd807a4c86
Đỗ Minh Hùng <92613966+DoMinhHHung@users.noreply.github.com>
2026-09-04 16:00:48 +0700

    beebox-fix: drop unused imports in identity http router

--- beebox-identity/internal/interface/http/router.go
+++ beebox-identity/internal/interface/http/router.go
@@ -2,17 +2,10 @@ package httpapi
 
 import (
 	"context"
-	"crypto/subtle"
-	"encoding/json"
-	"errors"
-	"io"
 	"net/http"
-	"strings"
 
 	"github.com/DoMinhHHung/beebox/beebox-identity/internal/application"
 	"github.com/DoMinhHHung/beebox/beebox-identity/internal/domain"
-	"github.com/DoMinhHHung/beebox/libs/shared/apperror"
-	"github.com/google/uuid"
 )
 
 const (
